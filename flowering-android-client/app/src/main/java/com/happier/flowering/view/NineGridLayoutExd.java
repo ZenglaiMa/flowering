7404551ce6348d1dec93eb029f6f56c3a638ef72
马增来 <1303035373@qq.com>
2020-04-21 15:33:52 +0800

    finish picture switch

--- flowering-android-client/app/src/main/java/com/happier/flowering/view/NineGridLayoutExd.java
+++ flowering-android-client/app/src/main/java/com/happier/flowering/view/NineGridLayoutExd.java
@@ -1,21 +1,25 @@
 package com.happier.flowering.view;
 
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.happier.flowering.R;
+import com.youth.banner.Banner;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
 
 import java.util.List;
 
 /**
- * @ClassName
- * @Description
+ * @ClassName NineGridLayoutExd
+ * @Description 九宫格的显示以及图片的点击效果
  * @Author seven
  * @date 2020/4/21 9:26
  * @Version 0.1
@@ -42,22 +46,31 @@ public class NineGridLayoutExd extends NineGridLayout {
 
     @Override
     protected void onClickImage(int position, String url, List<String> urlList) {
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        View dialogView = inflater.inflate(R.layout.dialog_origin_image, null);
-        ImageView largeImage = dialogView.findViewById(R.id.m_large_image);
-        Glide.with(mContext).load(url).into(largeImage);
-        final AlertDialog dialog = new AlertDialog.Builder(mContext).create();
-        dialog.setView(dialogView);
-        dialog.getWindow().setBackgroundDrawableResource(android.R.color.background_dark);
-        dialog.show();
-
-        dialogView.setOnClickListener(new View.OnClickListener() {
+        View dialogView = LayoutInflater.from(mContext).inflate(R.layout.dialog_origin_image, null);
+        final Dialog dialog = new Dialog(mContext, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        Banner banner = dialogView.findViewById(R.id.m_large_image_banner);
+        banner.setImageLoader(new ImageLoader() {
             @Override
-            public void onClick(View v) {
+            public void displayImage(Context context, Object path, ImageView imageView) {
+                Glide.with(mContext).load(path).into(imageView);
+            }
+        });
+        banner.setImages(urlList);
+        banner.isAutoPlay(false);
+        banner.setBannerAnimation(Transformer.ZoomIn);
+        banner.setBannerAnimation(Transformer.ZoomOut);
+        banner.setOnBannerListener(new OnBannerListener() {
+            @Override
+            public void OnBannerClick(int position) {
                 if (dialog.isShowing()) {
-                    dialog.cancel();
+                    dialog.dismiss();
                 }
             }
         });
+        banner.start();
+        dialog.setContentView(dialogView);
+        Window dialogWindow = dialog.getWindow();
+        dialogWindow.setWindowAnimations(R.style.DialogStyle);
+        dialog.show();
     }
 }
