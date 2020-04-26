da0de5f876ce7bd276119cd09845a9c7bc53e727
your name <your email>
2020-04-27 00:35:38 +0800

    y

---
+++ flowering-android-client/app/src/main/java/com/happier/flowering/activity/PlantDetailActivity.java
@@ -0,0 +1,76 @@
+package com.happier.flowering.activity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.happier.flowering.R;
+import com.happier.flowering.entity.Plant;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+//植物详情页
+public class PlantDetailActivity extends AppCompatActivity {
+    private TextView plantName;
+    private ImageView drawBackImg;
+    private ImageView plantPic;
+    private TextView plantDesc;
+    private TextView plantFoster;
+    private Intent intent;
+    private TextView plantFamily;
+    private TextView plantSkill;
+
+    private Gson gson = new Gson();
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plantdetail_info);
+        drawBackImg = findViewById(R.id.yimg_drawback);
+        plantPic = findViewById(R.id.yimg_plantDetailImg);
+        plantDesc = findViewById(R.id.ytv_plantDesc);
+        plantFoster = findViewById(R.id.ytv_Foster);
+        plantName = findViewById(R.id.ytv_plantDetailName);
+        plantFamily = findViewById(R.id.ytv_plantFamilt);
+        plantSkill = findViewById(R.id.ytv_Skill);
+
+
+        intent = getIntent();
+        String plantInfo = intent.getStringExtra("plantInfo");
+        Log.e("plantInfo",plantInfo);
+        Plant plant = gson.fromJson(plantInfo,new TypeToken<Plant>() {
+        }.getType());
+
+        plantName.setText(plant.getNameCn());
+
+        drawBackImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Picasso.with(this).load(plant.getImg()).into(plantPic);
+
+        plantFamily.setText(plant.getFamily());
+
+        plantDesc.setText(plant.getDescription());
+
+        plantFoster.setText(plant.getConservation());
+
+        plantSkill.setText(plant.getSkill());
+
+
+    }
+}
---
+++ flowering-android-client/app/src/main/java/com/happier/flowering/view/MyScrollView.java
@@ -0,0 +1,12 @@
+package com.happier.flowering.view;
+
+import android.content.Context;
+import android.widget.ScrollView;
+
+public class MyScrollView extends ScrollView {
+    public MyScrollView(Context context) {
+        super(context);
+    }
+
+
+}
